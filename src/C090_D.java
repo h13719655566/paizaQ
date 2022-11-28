66ca4a05ea502f6da498b59bc0f1abcb35b094c0
188 <p129611@gmail.com>
2022-11-28 22:28:38 +0800

    added C090

--- src/C090_D.java
+++ src/C090_D.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class C090_D {
+import java.util.Scanner;
+
+public class C090_D {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+        int result = 0;
+        for(int i = 0; i < string.length(); i++) {
+            if(string.charAt(i) == '-') continue;
+            if(string.charAt(i) == '0') {
+                result += 12;
+            } else {
+                result += (int) string.charAt(i) - 46;
+            }
+        }
+        System.out.println(result * 2);
+    }
 }
